b4e068b675312d29971038e6839e337d5d436f6f
caitlin-rich <caitlinboagrich@gmail.com>
2021-06-15 22:49:47 -0400

    feat vulcan language

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-let trekWords = ['boldly', 'cardassian', 'enterprise', 'vulcan', 'earl grey', 'coffee', 'viewscreen', 'lcars', 'pointed ears', 'alpha', 'beta', 'gamma', 'delta', 'quadrant', 'voyager', 'defiant', 'deep space nine', 'spaceship', 'stars', 'space station', 'janeway', 'picard', 'sisko', 'burnham', 'kirk', 'archer', 'first officer', 'captain', 'engineering', 'operations', 'red shirt', 'astrometrics', 'command', 'blue', 'gold', 'red', 'red alert', 'phasers', 'set to stun', 'borg', 'klingon', 'andorian', 'tellarite', 'romulan', 'human', 'dolar', 'sit', 'amet', 'consectetur', 'adipeiscing', 'elit', 'etiam', 'ultricies', 'ex', 'id', 'ad', 'morbi', 'tristique', 'denectus', 'et', 'et', 'nec', 'orci', 'aliquam', 'facilisis', 'ipsum', 'tincidunt']
+//Dictionaries of Words
 
-let klingonWords = ['Jelik', 'jat\'y\'lyn', 'Jelik', 'jinaq', 'k\'adio', 'ka\'vek', 'kesh', 'kellicam', 'khiGos', 'korut', 'pekt', 'kolat chack tabak', 'kortar', 'kos\'karii', 'kyamo', 'Len\'mat', 'makh\'Cha', 'matleh', 'worf', 'martok', 'b\'elanna', 'mauk-to\'Vor', 'sto-vo\'Kor', 'mekvak', 'mok', 'mokTah', 'naDev ghoS', 'par\'Mach', 'par\'Mach\'kai', 'ouee naGah', 'petaQ', 'po\'tajg', 'Nuq\'nuh', 'qu\'valth', 'shuVak', 'sliVak', 'soh-chim', 'soweeTa', 'SuH', 'Suvwl', 't\'gla', 'tohzah', 'toruk doh', 'vang ghaH', 'tan', 'yej rhin', 'yiGhom aH', 'yinTahg', 'adanji', 'baH', 'baktag', 'bat\'leth', 'Bekk', 'cha\'Dich', 'Cho\'echu', 'DaH', 'deLak doh', 'd\'akturak', 'd\'blok', 'Dhak\'tah', 'Fek\'lhr', 'tagh', 'Forshak', 'ghoptu', 'ghos', 'gik\'tal', 'he\'HimaH', 'grethor', 'Ha\'DlbaH', 'Hijol', 'Hechu\'ghos', 'hur\'q', 'posuere', 'enim', 'metus', 'sit', 'amet', 'rutrum', 'diu', 'convallis', 'nisi', 'ligula', 'portitor', 'quam', 'curabiter', 'conubia', 'viverra', 'bibendum', 'libero', 'etiam']
+const trekWords = ['boldly', 'cardassian', 'enterprise', 'vulcan', 'earl grey', 'coffee', 'viewscreen', 'lcars', 'pointed ears', 'alpha', 'beta', 'gamma', 'delta', 'quadrant', 'voyager', 'defiant', 'deep space nine', 'spaceship', 'stars', 'space station', 'janeway', 'picard', 'sisko', 'burnham', 'kirk', 'archer', 'first officer', 'captain', 'engineering', 'operations', 'red shirt', 'astrometrics', 'command', 'blue', 'gold', 'red', 'red alert', 'phasers', 'set to stun', 'borg', 'klingon', 'andorian', 'tellarite', 'romulan', 'human', 'dolar', 'sit', 'amet', 'consectetur', 'adipeiscing', 'elit', 'etiam', 'ultricies', 'ex', 'id', 'ad', 'morbi', 'tristique', 'denectus', 'et', 'et', 'nec', 'orci', 'aliquam', 'facilisis', 'ipsum', 'tincidunt', 'dilithium', 'isoliner', 'warp', 'frontier', 'power', 'couplings', 'jeffries', 'console']
+
+const klingonWords = ['Jelik', 'jat\'y\'lyn', 'Jelik', 'jinaq', 'k\'adio', 'ka\'vek', 'kesh', 'kellicam', 'khiGos', 'korut', 'pekt', 'kolat chack tabak', 'kortar', 'kos\'karii', 'kyamo', 'Len\'mat', 'makh\'Cha', 'matleh', 'worf', 'martok', 'b\'elanna', 'mauk-to\'Vor', 'sto-vo\'Kor', 'mekvak', 'mok', 'mokTah', 'naDev ghoS', 'par\'Mach', 'par\'Mach\'kai', 'ouee naGah', 'petaQ', 'po\'tajg', 'Nuq\'nuh', 'qu\'valth', 'shuVak', 'sliVak', 'soh-chim', 'soweeTa', 'SuH', 'Suvwl', 't\'gla', 'tohzah', 'toruk doh', 'vang ghaH', 'tan', 'yej rhin', 'yiGhom aH', 'yinTahg', 'adanji', 'baH', 'baktag', 'bat\'leth', 'Bekk', 'cha\'Dich', 'Cho\'echu', 'DaH', 'deLak doh', 'd\'akturak', 'd\'blok', 'Dhak\'tah', 'Fek\'lhr', 'tagh', 'Forshak', 'ghoptu', 'ghos', 'gik\'tal', 'he\'HimaH', 'grethor', 'Ha\'DlbaH', 'Hijol', 'Hechu\'ghos', 'hur\'q', 'posuere', 'enim', 'metus', 'sit', 'amet', 'rutrum', 'diu', 'convallis', 'nisi', 'ligula', 'portitor', 'quam', 'curabiter', 'conubia', 'viverra', 'bibendum', 'libero', 'etiam']
+
+const vulcanWords = ['abmarkan', 'abomseya', 'aborau', 'abrash', 'eiktra', 'ashayam', 'spock', 'tuvok', 'vorik', 'taurik', 'michael', 'abru-mal-hinek', 'abru-nalatra', 'afersu', 'afsakau', 'ah', 'Ah\'rak', 'ahkhan', 'ahm', 'akali', 'alem-masu', 'aluk-kum-fak', 'an-prele', 'ashaya', 'ashau', 'ausham', 'bahan', 'bar-got', 'batai', 'besan', 'betaya', 'beyik', 'bikuv', 'bezhun-thekek', 'bol-tav', 'bolaya', 'c\'hik', 'cha', 'chakh', 'chkariya', 'czar\'ak', 'chorka', 'da-kuvau', 'da-fek', 'nel', 'dafel', 'dahrik', 'dash-pim-tersayek', 'dator', 'din-tor', 'dirik', 'donku', 'dungau-sarlah-nash-veh', 'dvatai', 'dvunei', 'dzhinaya', 'eik-morik', 'eikan', 'eik-vath', 'eik-mor-lap', 'ek\'', 'el\'mish', 'eimvuvak', 'eshikh', 'etwei', 'fa-wak-glansu', 'faf-kur', 'fah-yat', 'fal', 'fam-ketak-has', 'fan-veh', 'feihan', 'fiterpaya', 'fnalan', 'fupas', 'gad-ha-gel', 'galu-thorshek', 'galupik', 'gla-tor-dular', 'glu-masutra-vel', 'guhfik', 'guhsh', 'guvik', 'ha-fek', 'ha\'su', 'hakausu', 'hal-tor', 'haya', 'haseret', 'has-hassu', 'hesvulaya', 'igen-wesh-sahriv', 'ihnik', 'ihsek-kesek', 'im-tukh', 'ip-sut', 'is-fam', 'izh-hal-tor', 'jasif', 'jarel', 'ka-zehlik sayek', 'kadvin', 'kae', 'kafeh', 'kaht', 'kaiden', 'kai-ros']
+
+//Generates the paragraphs - sentence functionality coming soon!! 
 
 const paragraphGenerator = (num = 1, dictionary = trekWords) => {
 
@@ -15,8 +21,9 @@ const paragraphGenerator = (num = 1, dictionary = trekWords) => {
           let firstLetter = randomWord[0].toUpperCase()
           let restOfLetters = randomWord.slice(1)
           paragraph.push(firstLetter + restOfLetters)
-        }
-          paragraph.push(randomWord)        
+        } else {
+          paragraph.push(randomWord)  
+        }      
       }
       let joined = paragraph.join(' ') + '.'
       allParagraph.push(joined)
@@ -28,14 +35,14 @@ const paragraphGenerator = (num = 1, dictionary = trekWords) => {
   }
 
 
-
-let submitButton = document.getElementById('paragraphsubmit')
+//Integration with HTML
 
 let dictionaryChoice = trekWords;
+let submitButton = document.getElementById('paragraphsubmit')
 
-var checkbox = document.querySelector("input[name=klingon]");
-
-checkbox.addEventListener('change', function() {
+//Checkboxes for choice of language
+var checkboxklingon = document.querySelector("input[name=klingon]");
+checkboxklingon.addEventListener('change', function() {
     if (this.checked) {
         dictionaryChoice = klingonWords;
     } else {
@@ -43,6 +50,16 @@ checkbox.addEventListener('change', function() {
     }
 });
 
+var checkboxvulcan = document.querySelector("input[name=vulcan]");
+checkboxvulcan.addEventListener('change', function() {
+    if (this.checked) {
+        dictionaryChoice = vulcanWords;
+    } else {
+        dictionaryChoice = trekWords;
+    }
+});
+
+//Submits user request for amount of paragraphs and generates text
 submitButton.addEventListener('click', function(){
 
 
